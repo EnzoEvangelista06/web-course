c813384434a6085d24c7474a383c1c30c112191c
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2025-11-16 22:21:27 -0300

    Aula Básica - Comentários

---
+++ Basico/aula02.js
@@ -0,0 +1,8 @@
+// Isto é um comentário de uma linha. Ele é ignorado pelo interpretador JavaScript.
+
+/**Isto
+ * é um comentário
+ * de múltiplas linhas.
+ */
+
+console.log('Olá, Mundo!'); // Exibe uma mensagem no console
