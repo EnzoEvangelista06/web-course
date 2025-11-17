331f4be4aa936111e7388462f02798f1cbca9e7d
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2025-11-17 07:47:00 -0300

    Exercício Básico - Treinando com variáveis

---
+++ Basico/exercicios/02.js
@@ -0,0 +1,9 @@
+const nome = 'Carlos';
+const sobrenome = 'Silva';
+const idade = 28;
+const peso = 75;
+const altura = 1.8;
+
+const IMC = peso / (altura * altura);
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}kg, tem ${altura}m de altura e seu IMC é: ${IMC}.`);
