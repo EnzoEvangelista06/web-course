7a8c52afad496a6437f9b2d568c3f942666ca904
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2025-11-16 22:17:42 -0300

    Aula Básica - Entendendo como funciona a função

---
+++ Basico/aula01.js
@@ -0,0 +1,14 @@
+// A função `console.log` é utilizada para imprimir mensagens no console do navegador ou no terminal, facilitando a depuração e o acompanhamento do fluxo do programa.
+// Podemos exibir textos e númmeros, separados por vírgulas, em uma única chamada ao console.log.
+console.log('Olá, mundo! Este é o meu primeiro código em JavaScript.');
+
+// Outras maneiras de exibir textos.
+console.log("Aprendendo JavaScript passo a passo.");
+console.log(`JavaScript é uma linguagem de programação versátil e poderosa.`);
+
+// Exibindo números inteiros e decimais.
+console.log(42);
+console.log(3.14159);
+
+// Exibindo múltiplos valores em uma única chamada.
+console.log('Números favoritos:', 7, 14, 21, 28);
