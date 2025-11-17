32c2a3c88d140dd4c78c5bda7cce545c1949bde7
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2025-11-17 08:15:06 -0300

    Aula Básica - Tipos primitivos

---
+++ Basico/aula06.js
@@ -0,0 +1,12 @@
+// Tipos de dados: string, number, boolean, undefined, null
+const nome = 'Luiz'; // String
+const idade = 2; // Number - Int
+const peso = 10.5; // Number - Float
+const eCrianca = true; // Boolean
+
+const nomeAluno = undefined; // undefined: aponta para local nenhum (não use isto)
+const sobrenomeAluno = null; // null: aponta para local nenhum (use isto)
+
+// `typeof`: Verificando o tipo das variáveis
+console.log(typeof nome, nome);
+console.log(typeof sobrenomeAluno, sobrenomeAluno);
