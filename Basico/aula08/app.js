4da46eed3241ce5b357b6f5efe006852a5c34e8e
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2025-11-17 15:18:01 -0300

    Aula Básica - Entendendo o , o  e o . Praticando com exercício também

---
+++ Basico/aula08/app.js
@@ -0,0 +1,9 @@
+// Exibe um alerta no navegador.
+window.alert('Olá!');
+
+// Exibe um prompt no navegador.
+const nome = prompt('Informe o seu nome: ');
+const idade = Number(prompt('Informe a sua idade: '));
+const maiorIdade = confirm('Em seu país, você é maior de idade?');
+
+console.log(nome, idade, maiorIdade);
---
+++ Basico/exercicios/03/app.js
@@ -0,0 +1,6 @@
+const numero = Number(prompt('Informe um número: '));
+const outroNumero = Number(prompt('Informe outro número: '));
+
+const adicao = numero + outroNumero;
+
+alert(`A soma entre os valores informados, é: ${adicao}`);
