ef0bf967f2bdc9375eec340ca3f0d3295bc07de1
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2025-11-16 22:32:09 -0300

    Aula Básica - Declarando variáveis

---
+++ Basico/aula03.js
@@ -0,0 +1,16 @@
+// Variáveis são utilizadas para armazenar dados que podem ser alterados durante a execução do programa.
+
+// Não podemos criar variáveis com:
+// Números (1nome)
+// Palavras reservadas (var, let, console)
+// Espaços ou traços (nome completo, nome-completo)
+// Começar com maiúscula (Nome)
+// Caracteres especiais (!, @, #, $, %, etc)
+
+let nome = 'João';
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000,', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve um filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
