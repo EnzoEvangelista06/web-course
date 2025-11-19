8b4ecf33d1cc37cf00867101e44f606bb0cc1d7c
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2025-11-19 07:48:42 -0300

    Aula Básica - Array - Final

--- Basico/aula12.js
+++ Basico/aula12.js
@@ -11,5 +11,44 @@ alunos[0] = 'Eduardo';
 console.log(alunos, alunos[0]);
 
 // Adicionando índice específico
-alunos[10] = 'Mariana'; // Adicionando elementos no índice 10.
+alunos[10] = 'Mariana'; // Adicionando elementos no índice 10. Obs.: isso não é recomendado.
 console.log(alunos, alunos[10]);
+
+// Tamanho do Array
+console.log(alunos.length);
+
+// Adicionando ao último elemento (método primitivo)
+alunos[alunos.length] = 'Luiza';
+console.log(alunos);
+
+alunos.push('Enzo'); // Método do array para adicionar elemento ao último índice.
+console.log(alunos);
+
+// adicionando no começo
+alunos.unshift('Anastácia');
+console.log(alunos);
+
+// removendo elemento do fim
+alunos.pop();
+// removendo do começo
+alunos.shift();
+
+console.log(alunos)
+
+// Deletando índice especifíco
+delete alunos[10];
+console.log(alunos);
+
+// undefined
+console.log(alunos[50]);
+
+alunos = ['Luiz', 'Maria', 'João'];
+alunos.push('Luiza');
+alunos.push('Eduardo');
+
+console.log(alunos[4]);
+
+console.log(alunos.slice(0, 3)); // fatiando array
+
+console.log(typeof alunos); // object -> sim, um array é um objeto na sua essência.
+console.log(alunos instanceof Array); // True -> alunos é uma instância de Array.
