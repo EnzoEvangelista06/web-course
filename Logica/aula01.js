78f66b6169102c378f8ace9955412ab7e9902f1c
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2025-11-19 14:53:56 -0300

    Aula de Lógica - Operadores de comparação

---
+++ Logica/aula01.js
@@ -0,0 +1,21 @@
+/** Operadores de comparação:
+ *      MAIOR QUE: >
+ *      MAIOR QUE OU IGUAL A: >=
+ *      MENOR QUE: <
+ *      MENOR QUE OU IGUAL A: <=
+ *      IGUAL: ==
+ *      IDÊNTICO: === (valor e tipo)
+ *      DIFERENTE: !=
+ *      NÃO IDÊNTICO: !== (valor e tipo)
+ *  
+ */
+
+console.log(10 >= 5);
+console.log(10 < 5);
+
+console.log('10' === 10); // false -> mesmo valor, diferente no tipo
+console.log('10' == 10); // true -> valores iguais
+
+console.log('10' != 10); // false -> valores iguais 
+console.log('10' !== 10); // true -> valores iguais, tipos diferentes
+
