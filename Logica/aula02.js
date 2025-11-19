02bc15a16fae0e3a937e70f95891d61f781fa9fa
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2025-11-19 15:08:17 -0300

    Aula de Lógica - Operadores lógicos

---
+++ Logica/aula02.js
@@ -0,0 +1,17 @@
+/** Operadores lógicos:
+ *      && -> AND -> E
+ *      || -> OR -> OU
+ *      ! -> NOT -> NÃO (inversão)
+ *  
+ */
+
+console.log(10 >= 10 && 15 < 10); // true -> Todas as comparações são verdadeira.
+console.log(10 > 10 && 15 < 10); // false -> Apenas uma comparação é verdadeira.
+
+console.log(10 > 10 || 15 < 10); // true -> uma expressão é verdadeira.
+
+// inversão de resultado
+console.log(!(10 > 10 || 5 === 9)); // true -> fizemos a inversão do resuldo.
+
+
+
