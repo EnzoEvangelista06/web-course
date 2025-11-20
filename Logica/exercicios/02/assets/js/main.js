6177e47f356af61e7e9c9f7f83b0fba3d4ea0717
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2025-11-20 18:15:52 -0300

    Exercício de Lógica - Manipulando datas - Outra solução

--- Logica/exercicios/02/assets/js/main.js
+++ Logica/exercicios/02/assets/js/main.js
@@ -2,35 +2,37 @@ const saida = document.querySelector('section.container h1');
 
 const data = new Date();
 
-const mesAno = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+// const mesAno = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
 
-let diaSemana;
-switch (data.getDay()) {
-    case 0:
-        diaSemana = 'Domingo';
-        break;
-    case 1:
-        diaSemana = 'Segunda-feira';
-        break;
+// let diaSemana;
+// switch (data.getDay()) {
+//     case 0:
+//         diaSemana = 'Domingo';
+//         break;
+//     case 1:
+//         diaSemana = 'Segunda-feira';
+//         break;
 
-    case 2:
-        diaSemana = 'Terça-feira';
-        break;
+//     case 2:
+//         diaSemana = 'Terça-feira';
+//         break;
 
-    case 3:
-        diaSemana = 'Quarta-feira';
-        break;
-    case 4:
-        diaSemana = 'Quinta-feira';
-        break;
-    case 5:
-        diaSemana = 'Sexta-feira';
-        break;
-    case 6:
-        diaSemana = 'Sábado';
-        break;
-    default:
-        break;
-}
+//     case 3:
+//         diaSemana = 'Quarta-feira';
+//         break;
+//     case 4:
+//         diaSemana = 'Quinta-feira';
+//         break;
+//     case 5:
+//         diaSemana = 'Sexta-feira';
+//         break;
+//     case 6:
+//         diaSemana = 'Sábado';
+//         break;
+//     default:
+//         break;
+// }
 
-saida.innerText = `${diaSemana}, ${data.getDate()} de ${mesAno[data.getMonth()]} de ${data.getFullYear()} - ${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`;
+// saida.innerText = `${diaSemana}, ${data.getDate()} de ${mesAno[data.getMonth()]} de ${data.getFullYear()} - ${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`;
+
+saida.innerHTML = data.toLocaleDateString('pt-BR', { dateStyle: 'full'});
