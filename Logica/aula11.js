3a7d9f19641a8a90d07367917ce4b47581d0f0f3
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2025-11-20 20:05:27 -0300

    Aula de Lógica - Laço de repetição - for

---
+++ Logica/aula11.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(`Linha: ${i}`);
+}
+
+const frutas = ['Maçã', 'Pera', 'Uva', 'Banana', 'Goiaba', 'Manga'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
