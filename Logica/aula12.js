2ffe736bfe664153d87221236c259c2c8f3047b2
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2025-11-21 08:14:48 -0300

    Aula de Lógica - For in

---
+++ Logica/aula12.js
@@ -0,0 +1,14 @@
+const frutas = ['Pera', 'Maça', 'Uva'];
+
+for (let i in frutas) {
+    console.log(i, frutas[i]);
+}
+
+const aluno = {
+    nome: 'João',
+    idade: 16,
+}
+
+for (let i in aluno) {
+    console.log(i, aluno[i]);
+}
