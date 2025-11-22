4ad851a82ba23a24123d6505cb865180c7df21a1
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2025-11-22 08:36:16 -0300

    Exercício de Lógica - Exercício proposto #01

---
+++ Logica/exercicios/05.js
@@ -0,0 +1,6 @@
+// Uma função que receba dois números e retorne o maior entre eles.
+// arrow function
+const maiorNumero = (x, y) => x > y ? x : y;
+
+console.log(maiorNumero(5, 10));
+console.log(maiorNumero(34, 12));
